18af4ccc349ffb3e78734ff78cdd0d4090e2907f
gwill-9 <gwill9@illinois.edu>
2024-07-24 22:04:24 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ let maxYearGlobal = 2014;
 let Listener = false;
 
 
-console.log('version 1.50');
+console.log('version 1.51');
     
 // Function to update the chart based on selected year range
 function updateChart(data, minYear, maxYear) {
@@ -208,8 +208,8 @@ function renderChart(countryArray, scatterData, filteredData) {
             .call(d3.axisLeft(yScatter))
             .append("text")
             .attr("fill", "#000")
-            .attr("x", scatterHeight / 2)
-            .attr("y", -margin.left)
+            .attr("x", 0)
+            .attr("y", 0)
             .attr("text-anchor", "middle")
             .attr("transform", "rotate(-90)")
             .text("C02 From Electric Power Generation And Heating (kt Per capita)");
